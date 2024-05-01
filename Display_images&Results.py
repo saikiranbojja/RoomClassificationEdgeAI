a0f7efae92bc73671924c9f0b92d52d1e518b116
saikiranbojja <59079735+saikiranbojja@users.noreply.github.com>
2024-05-01 20:49:34 +0530

    Initial commit

---
+++ Display_images&Results.py
@@ -0,0 +1,75 @@
+import numpy as np
+import serial
+import sys
+import uuid
+from PIL import Image
+import cv2
+
+port = 'COM14'
+baudrate = 115600
+
+# Initialize serial port
+ser = serial.Serial()
+ser.port     = port
+ser.baudrate = baudrate
+ser.open()
+ser.reset_input_buffer()
+
+label  = "test"
+width  = 1
+height = 1
+num_ch = 3
+
+image = np.empty((height, width, num_ch), dtype=np.uint8)
+
+
+def serial_readline():
+    data = ser.readline() 
+    return data.decode("utf-8").strip()
+
+while True:
+    data_str = serial_readline()
+
+    if str(data_str) == "<image>":
+        w_str = serial_readline()
+        h_str = serial_readline()
+        w = int(w_str)
+        h = int(h_str)
+        if w != width or h != height:
+            # print("Resizing numpy array")
+            if w * h != width * height:
+                image.resize((h, w, num_ch))
+            else:
+                image.reshape((h, w, num_ch))
+            width  = w
+            height = h
+        print("Reading frame:", width, height)
+        for i in range(0, width * height * num_ch):
+            c = int(i % num_ch)
+            y = int((i / num_ch) / width)
+            x = int((i / num_ch) % width)
+            data_str = serial_readline()
+            image[y][x][c] = int(data_str)
+        data_str = serial_readline()
+        if str(data_str) == "</image>":
+            title = ser.readline().decode('utf-8').strip()
+            # print("Captured frame")
+            crop_area = (0, 0, height, height)
+            image_pil     = Image.fromarray(image)
+            image_cropped = image_pil.crop(crop_area)
+            zoom_factor = 15
+            image_cropped = image_cropped.resize((image_cropped.width * zoom_factor, image_cropped.height * zoom_factor))
+            image_cropped = cv2.cvtColor(np.array(image_cropped), cv2.COLOR_RGB2BGR) 
+            # image_cropped.show()
+            cv2.imshow(title, np.array(image_cropped))
+            cv2.waitKey(4000)
+            cv2.destroyAllWindows()
+
+
+
+
+
+  
+           
+        else:
+            print("Error capture image")
